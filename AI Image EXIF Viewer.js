b1f188c140d1cf3337ce78277573f57e3fd55585
nyqui <67160376+nyqui@users.noreply.github.com>
2023-04-21 04:14:45 +0900

    Update AI Image EXIF Viewer.js

--- AI Image EXIF Viewer.js
+++ AI Image EXIF Viewer.js
@@ -6,7 +6,7 @@
 // @match       https://arca.live/b/hypernetworks*
 // @match       https://arca.live/b/aiartreal*
 // @match       https://arca.live/b/aireal*
-// @version     1.10.1-alpha.3
+// @version     1.10.1
 // @author      nyqui
 // @require     https://greasyfork.org/scripts/452821-upng-js/code/UPNGjs.js?version=1103227
 // @require     https://cdn.jsdelivr.net/npm/casestry-exif-library@2.0.3/dist/exif-library.min.js
